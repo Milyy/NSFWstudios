188edc841f2542dd58292f372dac46e9b8fac054
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2014-01-14 01:31:39 -0300

    first commit, added gems and assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NSFWstudios::Application.config.secret_key_base = '50b22e8cd2040f04488df2d9c99338cd61fbf658a54ce2d2b6a5b60d30961279970d5c73f6e77694f74037189462f4c04d0e112e39ae4cf885a8a19ce123a5aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NSFWstudios::Application.config.session_store :cookie_store, key: '_NSFWstudios_session'
